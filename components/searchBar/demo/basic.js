c5aee7651680138f05abc5c1623e3a1e86000afb
刘金萌 <1321839748ljm@gmail.com>
2019-09-20 19:51:30 +0800

    feat: searchbar demo

--- components/searchBar/demo/basic.js
+++ components/searchBar/demo/basic.js
@@ -2,30 +2,43 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { SearchBar } from '../..'
 
-export default () => (
-    <View style={styles.warp}>
-        <Text style={styles.title}>
-            未输入状态
-        </Text>
-        <SearchBar type="radius" placeholder="搜索" />
-        <Text style={styles.title}>
-            输入中状态
-        </Text>
-        <SearchBar type="radius" value="打卡助手" />
-        <Text style={styles.title}>
-            输入结束状态
-        </Text>
-        <SearchBar type="radius" value="打卡助手" />
-        <Text style={styles.title}>
-            未输入状态
-        </Text>
-        <SearchBar placeholder="搜索" />
-        <Text style={styles.title}>
-            输入结束状态
-        </Text>
-        <SearchBar value="打卡助手" />
-    </View>
-)
+export default class SearchBarExample extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            value: '打卡助手',
+        }
+    }
+    render() {
+        const { value } = this.state
+        return (
+            <View style={styles.warp}>
+                <Text style={styles.title}>
+                    未输入状态
+                </Text>
+                <SearchBar type="radius" placeholder="搜索" />
+                <Text style={styles.title}>
+                    初始值
+                </Text>
+                <SearchBar type="radius" defaultValue="打卡助手" />
+                <Text style={styles.title}>
+                    通栏搜索框 初始值
+                </Text>
+                <SearchBar defaultValue="打卡助手" />
+                <Text style={styles.title}>
+                    搜索
+                </Text>
+                <SearchBar
+                    value={value}
+                    placeholder="搜索"
+                    onSubmit={(val) => console.log(val)}
+                    onCancel={() => this.setState({ value: '' })}
+                    onChange={(val) => this.setState({ value: val })}
+                />
+            </View>
+        )
+    }
+}
 
 const styles = StyleSheet.create({
     warp: {
